01a4ac419ebb3c5c96ce57ac3e9ced7d9a2cfcd3
Amin Mamandipoor <a972m888@sapphire.ittc.ku.edu>
2024-03-27 16:26:25 -0500

    unix working

--- data-caching-rs/client/client-skt.sh
+++ data-caching-rs/client/client-skt.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 
-if [ "$1" = "help" ]; then
-    echo "Usage: clinet-skt.sh [mode] [workers] [connections] [rps]"
-    echo "      workers:     number of workers"
-    echo "      mode:        mode to run memcached client"
-    echo "      connections: number of connections"
-    echo "      RPS:         number requests per second"
-
-    exit 0
-fi
-#sudo killall -9 loader 
-
+ sudo killall -9 loader 
 # max throughput
 if [ "$1" == "th" ]; then
-    ./loader -a ./twitter_dataset_64  -s unixScoket.txt -g 0.8 -c $3 -w $2 -T 1 -D 23040 -X
+    ./loader -a ./twitter_dataset_2x  -s unixScoket.txt -g 0.8 -c 1 -w 1 -T 1 -D 730 -X
 # rps
 elif [ "$1" == "rps" ]; then
-    ./loader -a ./twitter_dataset_64  -s unixScoket.txt -g 0.8 -c $3 -w $2 -T 1 -D 23040 -r $4 -X
-# warmup
-elif [ "$1" == "w" ]; then
-    ./loader -a ./twitter_dataset_64  -s unixScoket.txt -c $3 -w $2 -S 1 -T 1 -j -D  23040 -X
+    ./loader -a ./twitter_dataset_2x  -s unixScoket.txt -g 0.8 -c 1 -w 1 -T 1 -D 730 -r 1000 -X
 # scale&warmup
 elif [ "$1" == "sw" ]; then
-    ./loader -a ./twitter_dataset_unscaled -o ./twitter_dataset_64 -s unixScoket.txt -c $3 -w $2 -S 64 -T 1 -j -D  23040 -X
-
+    ./loader -a  ./twitter_dataset_2x -s unixScoket.txt -c 1 -w 1 -S 1 -T 1 -j -D  730 -X 
 else
-    echo "Invalid option. Please specify either 'th' or 'sw' or 'rps' or 'w'  as the first argument."
+    echo "Invalid option. Please specify either 'th' or 'sw' or 'rps' as the first argument."
 fi
 
 
+# Finally, we find that thread-pinning and interrupt routing based on network flow-affinity resolve much of memcached’s vulnerability to load imbalance
